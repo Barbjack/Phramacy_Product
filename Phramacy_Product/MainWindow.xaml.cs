ae40104612c0c8762e38d5491e806d40bc16b5d3
Developer <Developer@MSI>
2025-09-15 20:14:11 +0530

    This is commit for update

--- Phramacy_Product/MainWindow.xaml.cs
+++ Phramacy_Product/MainWindow.xaml.cs
@@ -1,10 +1,16 @@
-﻿using Phramacy_Product.Views.Components;
-using Phramacy_Product.Views.Sales; // Add this using statement
+﻿using Newtonsoft.Json;
+using Phramacy_Product.Views.Components;
 using System;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Net.NetworkInformation;
+
 
 namespace Phramacy_Product
 {
@@ -17,7 +23,15 @@ namespace Phramacy_Product
             InitializeComponent();
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        public enum LoginStatus
+        {
+            Success,
+            InvalidCredentials,
+            DatabaseError,
+            LicenseError,
+            InternetError
+        }
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             string mobile = MobileNumberTextBox.Text;
             string password = PasswordBox.Password;
@@ -28,24 +42,34 @@ namespace Phramacy_Product
                 return;
             }
 
-            if (AuthenticateUser(mobile, password))
+            LoginStatus status = await AuthenticateUser(mobile, password);
+
+            switch (status)
             {
-                Dashboard dashboardWindow = new Dashboard();
-                this.Close();
-                dashboardWindow.Show();
-                //MessageBox.Show("Logged in Successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-                Clear_Form();
-            }
-            else
-            {
-                MessageBox.Show("Invalid mobile number or password.", "Login Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                case LoginStatus.Success:
+                    Dashboard dashboardWindow = new Dashboard();
+                    this.Close();
+                    dashboardWindow.Show();
+                    Clear_Form();
+                    break;
+                case LoginStatus.InvalidCredentials:
+                    MessageBox.Show("Invalid mobile number or password.", "Login Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                case LoginStatus.LicenseError:
+                       break;
+                case LoginStatus.DatabaseError:
+                    break;
+                case LoginStatus.InternetError:
+                    break;
+                default:
+                    MessageBox.Show("An unknown error occurred.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
             }
         }
 
-        private bool AuthenticateUser(string mobile, string password)
+        private async Task<LoginStatus> AuthenticateUser(string mobile, string password)
         {
-            bool isAuthenticated = false;
-            string query = "SELECT * FROM pharmacy_profile WHERE mobile = @mobile AND password = @password";
+            string query = "SELECT id, pharmacist_name, expiryDate, license_key, email FROM pharmacy_profile WHERE mobile = @mobile AND password = @password";
 
             try
             {
@@ -62,20 +86,148 @@ namespace Phramacy_Product
                         if (reader.Read())
                         {
                             GlobalData.LoggedInUser = reader["pharmacist_name"].ToString();
-                            GlobalData.userId = (int)reader["Id"];
-                            isAuthenticated = true;
+                            GlobalData.userId = (int)reader["id"];
+                            string licenseKey = reader["license_key"] as string;
+                            string email = reader["email"].ToString();
+                           
+                            if (string.IsNullOrEmpty(licenseKey))
+                            {
+                                if (!await CreateAndSaveLicense(GlobalData.LoggedInUser, email))
+                                {
+                                    return LoginStatus.LicenseError;
+                                }
+                            }
+                            else
+                            {
+                                DateTime? expiryDate = reader["expiryDate"] as DateTime?;
+                                if (expiryDate == null || expiryDate.Value.Date < DateTime.Now.Date)
+                                {
+                                    if (!await ValidateLicense(licenseKey))
+                                    {
+                                        return LoginStatus.LicenseError;
+                                    }
+                                }
+                            }
+                            reader.Close();
+                            return LoginStatus.Success;
                         }
+                        else
+                        {
+                            return LoginStatus.InvalidCredentials;      }
                     }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Database error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                 MessageBox.Show($"Database error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return LoginStatus.DatabaseError;
             }
-
-            return isAuthenticated;
         }
 
+        private async Task<bool> CreateAndSaveLicense(string name, string email)
+        {
+            
+            using (var client = new HttpClient())
+            {
+                var requestBody = new
+                {
+                    name = name,
+                    email = email,
+                    plan = "yearly",
+                    duration = 365
+                };
+                var content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync("https://quickrxbill.com/api/create_license.php", content);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
+
+                    if (jsonResponse.success == true)
+                    {
+                        string newLicenseKey = jsonResponse.license_key;
+                        DateTime expiryDate = jsonResponse.expiry;
+                        UpdateLocalLicense(newLicenseKey, expiryDate);
+                         return true;
+                    }
+                    else
+                    {
+                         return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Please connect to internet.", "Connection Error");
+                    return false;
+                }
+            }
+        }
+        private async Task<bool> ValidateLicense(string licenseKey)
+        {
+            
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    string url = $"https://quickrxbill.com/api/validate.php?key={licenseKey}";
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
+
+                    if (jsonResponse.success == true && jsonResponse.message== "License valid")
+                    {
+                        DateTime expiryDate = jsonResponse.expiry;
+                        UpdateLocalLicense(licenseKey, expiryDate);
+                        MessageBox.Show("License is valid and active.", "Success");
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("License is invalid or expired. Please contact support.", "Warning");
+                        UpdateLocalLicense(null, DateTime.MinValue);
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Please connect to the internet: {ex.Message}", "Error");
+                    return false;
+                }
+            }
+        }
+
+        private void UpdateLocalLicense(string licenseKey, DateTime expiryDate)
+        {
+            if (!string.IsNullOrEmpty(licenseKey))
+            {
+                string query = "UPDATE pharmacy_profile SET license_key = @licenseKey, expiryDate = @expiryDate WHERE Id = @userId";
+
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@licenseKey", licenseKey);
+                            command.Parameters.AddWithValue("@expiryDate", expiryDate);
+                            command.Parameters.AddWithValue("@userId", GlobalData.userId);
+
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to update local license: {ex.Message}", "Error");
+                    // Remove 'return;' as this method is void.
+                }
+            }
+        }
+        
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
